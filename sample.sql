8859c48915c272a9f6a00d1950aca0b892cff7c7
akauble <austinkauble@gmail.com>
2016-11-23 15:26:36 -0600

    correct insert syntax

--- sample.sql
+++ sample.sql
@@ -1,4 +1,4 @@
-INSERT INTO employee VALUES (
+INSERT INTO employee VALUES
 	(382901, 'Frank', 'Cessna', 1, 'Captain', 1, 18370),
 	(388929, 'Sue', 'Grumman', 1, 'Captain', 1, 14799),
 	(483792, 'John', 'Piper', 1, 'First Officer', 0, 11870),
@@ -8,10 +8,9 @@ INSERT INTO employee VALUES (
 	(372819, 'Simon', 'Said', 2, 'Junior', 1, 2887),
 	(776573, 'Janet', 'Johnson', 2, 'Senior', 1, 7654),
 	(111980, 'Hillary', 'Trump', 2, 'Senior', 1, 13101),
-	(485932, 'Donald', 'Clinton', 2, 'Senior', 1, 8876)
-);
+	(485932, 'Donald', 'Clinton', 2, 'Senior', 1, 8876);
 
-INSERT INTO certification VALUES (
+INSERT INTO certification VALUES
 	(382901, 'B747'),
 	(382901, 'B737'),
 	(388929, 'DC9'),
@@ -21,10 +20,9 @@ INSERT INTO certification VALUES (
 	(887362, 'B747'),
 	(887362, 'A380'),
 	(887362, 'B737'),
-	(123492, 'A380')
-);
+	(123492, 'A380');
 
-INSERT INTO equipment VALUES (
+INSERT INTO equipment VALUES
 	('N47892', 300, 2, 3, 'B747'),
 	('N21094', 180, 2, 3, 'B737'),
 	('N009', 190, 2, 3, 'MD80'),
@@ -36,18 +34,14 @@ INSERT INTO equipment VALUES (
 	('N09982', 300, 2, 3, 'B747'),
 	('N1029', 180, 2, 3, 'B737'),
 	('N77837', 290, 2, 3, 'A380'),
-	('N77994', 190, 2, 2, 'MD80')
-);
+	('N77994', 190, 2, 2, 'MD80');
 
-INSERT INTO flights VALUES (
+INSERT INTO flights VALUES
 	(454, '2016-11-19', 1129, 'DALLAS', 'PARIS', '07:00:00', '17:30:00', 'N3029', 123492, 887362, null, 99872, 485932, 473829),
-	(5540, '2016-11-21', 899, 'PARIS', 'CHICAGO', '16:00:00', '05:00:00', 'N47892', 382901, 887362, null, 776573, 372819, 485932)
-);
+	(5540, '2016-11-21', 899, 'PARIS', 'CHICAGO', '16:00:00', '05:00:00', 'N47892', 382901, 887362, null, 776573, 372819, 485932);
 
-INSERT INTO customer VALUES (
-	(1, 'Susan', Miller)
-);
+INSERT INTO customer VALUES
+	(1, 'Susan', Miller);
 
-INSERT INTO reservation VALUES (
-	('B4091', 5540, 1, 899)
-);
+INSERT INTO reservation VALUES
+	('B4091', 5540, 1, 899);
