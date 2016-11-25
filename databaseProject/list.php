b1770af3df34f71397209e8fb975728ca814155b
akauble <austinkauble@gmail.com>
2016-11-25 00:55:02 -0600

    select from correct table

--- databaseProject/list.php
+++ databaseProject/list.php
@@ -94,7 +94,7 @@
         $returnDate_search = $_POST["returnDate"]; 
         $price_search = $_POST["price"]; 
         
-        $statement = mysqli_prepare($conn, "SELECT * FROM database WHERE from, to, departureDate, returnDate, price LIKE ?, ?, ?, ?, ?");
+        $statement = mysqli_prepare($conn, "SELECT * FROM flight WHERE from, to, departureDate, returnDate, price LIKE ?, ?, ?, ?, ?");
     
         
         
