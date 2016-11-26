a1317081a18db4fca516d235edbd3881bb513ca8
karlnuetzel <karlanuetzel@gmail.com>
2016-11-26 13:32:12 -0600

    Add files via upload

--- databaseProject/log.php
+++ databaseProject/log.php
@@ -11,7 +11,7 @@
         include("../secure/database.php");
         $conn = mysqli_connect(HOST,USERNAME,PASSWORD,DBNAME) or die("Connect Error " . mysqli_error($conn));
         
-        $result = mysqli_query($conn, "SELECT * from classes");
+        $result = mysqli_query($conn, "SELECT * from logging");
         echo "<table border=1>";
 
         while($field = mysqli_fetch_field($result)){
