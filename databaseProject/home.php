a44dbbe2c846c9135fde2bac894f6f9b4de8632b
karlnuetzel <karlanuetzel@gmail.com>
2016-11-27 14:59:37 -0600

    Add files via upload

--- databaseProject/home.php
+++ databaseProject/home.php
@@ -19,13 +19,6 @@
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-      
     <style>
         
         div img {
@@ -51,30 +44,11 @@
         </div>
         <div id="navbar" class="navbar-collapse collapse">
           <form class="navbar-form navbar-right">
-            <div class="form-group">
-              <input type="text" placeholder="Email" class="form-control">
-            </div>
-            <div class="form-group">
-              <input type="password" placeholder="Password" class="form-control">
-            </div>
-            <button type="submit" class="btn btn-success">Sign in</button>
           </form>
         </div><!--/.navbar-collapse -->
       </div>
     </nav>
-
-    <!-- Main jumbotron for a primary marketing message or call to action -->
-    <div class="jumbotron">
-      <div class="container">
-        <h1>
-            <br>
-            <div>
-            <img src="MissouriAirLogo1.gif" alt="Missouri Air" id="logo">
-            </div>
-          </h1>
-      </div>
-    </div>
-    
+      
     <div class="container">
         <h1>Employee functions go here..</h1>
         <hr>
