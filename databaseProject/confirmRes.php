b11bbc4e193df7f9a359b8895022014201a6c9c4
akauble <austinkauble@gmail.com>
2016-11-27 16:56:15 -0600

    customer debug

--- databaseProject/confirmRes.php
+++ databaseProject/confirmRes.php
@@ -97,6 +97,7 @@ bag and 5% sales tax.-->
 		<br>
 		Number of Bags: 
 		<select name="bags">
+    	<option value="0">0</option>
 			<option value="1">1</option>
 			<option value="2">2</option>
       <option value="3">3</option>	
