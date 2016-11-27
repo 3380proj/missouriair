8de3312a16c294351199bc192d30ccc83ba72dd2
karlnuetzel <karlanuetzel@gmail.com>
2016-11-27 16:56:16 -0600

    Add files via upload

--- databaseProject/home.php
+++ databaseProject/home.php
@@ -28,6 +28,8 @@
             if ($jobtype == 0){
                 
                 $empPosition = "Admin";
+                //header ("Location: admin.php");
+                
                 
             }else if ($jobtype == 1){
                 
@@ -103,6 +105,28 @@
     <br><br><br><br><br><br>
     <div class="container">
         <h1><?php echo $empPosition; ?></h1>
+        <?php
+            statement = mysqli_prepare($conn, "SELECT * FROM flight WHERE CONCAT(pilot_1, ' ', pilot_2, ' ', pilot_3, ' ', att_1, ' ', att_2, ' ', att_3) LIKE ?");
+            mysqli_stmt_bind_param($statement, "i", $empID);
+            if(mysqli_stmt_execute($statement)){
+                mysqli_stmt_bind_result($statement,$number,$departureDate,$price,$origin,$dest,$dep,$arr,$aircraft,$pilot_1,$pilot_2,$pilot_3,$att_1,$att_2,$att_3);
+                echo "<table class=\"table\">\n";
+                echo "<thead>\n\t<tr>\n\t\t<th>Origin</th>\n\t\t<th>Destination</th>\n\t\t<th>Date</th>\n\t\t<th>Departure</th>\n\t\t<th>Aircraft</th>\n\t</tr>\n</thead>\n";
+                while (mysqli_stmt_fetch($statement))
+                {
+                  echo "<tr>\n";
+                  echo "\t<td>" . $origin . "</td>\n";
+                  echo "\t<td>" . $dest . "</td>\n";
+                  echo "\t<td>" . $departureDate . "</td>\n";
+                  echo "\t<td>" . $dep . "</td>\n";
+                  echo "\t<td>" . $aircraft . "</td>\n"; 
+                  echo "</tr>\n";
+
+                }
+                echo "</table>\n";
+            }
+            mysqli_stmt_close($statement);
+        ?>
         <hr>
         <footer>
             <p>&copy; 2016 Missouri Air, Inc.</p>
