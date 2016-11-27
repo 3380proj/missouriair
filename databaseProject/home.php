4cebebbbb5da546736afe7290efa685c119562ca
karlnuetzel <karlanuetzel@gmail.com>
2016-11-27 16:04:33 -0600

    Add files via upload

--- databaseProject/home.php
+++ databaseProject/home.php
@@ -79,7 +79,7 @@
           <a class="navbar-brand" href="#"><img src="MissouriAirLogo2.jpg" style="width:100px;height:100px;"></a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
-            <p class="navbar-text"><?php echo "Welcome " . $fname . " " . $lname; ?></p>
+            <p class="navbar-text navbar-right"><?php echo "Welcome " . $fname . " " . $lname; ?></p>
             <form class="navbar-form navbar-right" method = "POST">
                 <button type="submit" name="logoutBtn" class="btn btn-danger navbar-right">Logout</button>
             </form>
