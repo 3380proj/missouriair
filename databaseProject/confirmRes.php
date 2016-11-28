c7ef5341a40fa36f33e0f6582181207fb5167efb
akauble <austinkauble@gmail.com>
2016-11-27 21:38:10 -0600

    remove echo

--- databaseProject/confirmRes.php
+++ databaseProject/confirmRes.php
@@ -169,7 +169,6 @@
 
         if (!isset($_POST['custID'])){
             if(mysqli_stmt_execute($cust_statement)){
-                echo $fname . " " . $lname;
                 $cust_id = mysqli_insert_id($conn); 
             } else {
                 echo "\nCustomer creation error occurred: " . mysqli_stmt_error($cust_statement);
