bdbc2eaeeddcef4a800a92c5944d6944eb3042ba
akauble <austinkauble@gmail.com>
2016-11-27 18:57:00 -0600

    log params

--- databaseProject/log_event.php
+++ databaseProject/log_event.php
@@ -1,14 +1,11 @@
 <?php
-    function log_event($action_type, $action_desc) {
+    function log_event($conn, $action_type, $action_desc, $flight_no, $user_cust, $user_emp) {
         $log_statement = mysqli_prepare($conn, "INSERT INTO logging (ip, action_date, action_time, action_type, action_desc, user_emp, user_cust, flight_num) VALUES (?, ?, ?, ?, ?, ?)");
-        mysqli_stmt_bind_param($res_statement, "sssiii", $ip, $date, $time, $action_type, $action_desc, $user_emp, $user_cust, $flight_num);
+        mysqli_stmt_bind_param($log_statement, "sssiii", $ip, $date, $time, $action_type, $action_desc, $user_emp, $user_cust, $flight_num);
 
         $ip = $_SERVER['REMOTE_ADDR'];
         $date = date("Y-m-d");
         $time = date("H:i");
-        $user_emp = isset($_SESSION['admin'])?$_SESSION['admin']:null;
-        $user_cust = isset($cust_id)?$cust_id:null;
-        $flight_num = isset($flight_no)?$flight_no:null;
 
         mysqli_stmt_execute($log_statement);
     }       
