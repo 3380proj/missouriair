d94a4dd0680b34a59bf058b77e549c6066889d7d
akauble <austinkauble@gmail.com>
2016-11-27 18:59:05 -0600

    log fix

--- databaseProject/log_event.php
+++ databaseProject/log_event.php
@@ -1,6 +1,6 @@
 <?php
     function log_event($conn, $action_type, $action_desc, $flight_no, $user_cust, $user_emp) {
-        $log_statement = mysqli_prepare($conn, "INSERT INTO logging (ip, action_date, action_time, action_type, action_desc, user_emp, user_cust, flight_num) VALUES (?, ?, ?, ?, ?, ?)");
+        $log_statement = mysqli_prepare($conn, "INSERT INTO logging (ip, action_date, action_time, action_type, action_desc, user_emp, user_cust, flight_num) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
         mysqli_stmt_bind_param($log_statement, "sssiii", $ip, $date, $time, $action_type, $action_desc, $user_emp, $user_cust, $flight_num);
 
         $ip = $_SERVER['REMOTE_ADDR'];
