ef56e21bfe057f6d11eb9d45d77967d2d9cf0351
Jonathan Dusenberry <Jonathan Dusenberry>
2016-11-27 22:06:54 -0600

    this might be done

--- databaseProject/admin_certification.php
+++ databaseProject/admin_certification.php
@@ -118,8 +118,8 @@
                       echo "<tr>\n";
                       echo "\t<td>" . $emp_id . "</td>\n";
                       echo "\t<td>" . $equipment . "</td>\n";
-                      echo "\t<td><form action=\"admin_certification_edit.php\"><button name=\"Edit\" type=\"submit\" value=\"{$number}\" class=\"btn btn-secondary\">Edit</button></form></td>\n";
-                      echo "\t<td><form action='' method='POST'><button name=\"delete\" type=\"submit\" value=\"{$number}\" class=\"btn btn-secondary\">Delete</button></form></td>\n";
+                      echo "\t<td><form action=\"admin_certification_edit.php\"><button name=\"Edit\" type=\"submit\" class=\"btn btn-secondary\">Edit</button></form></td>\n";
+                      echo "\t<td><form action='' method='POST'><button name=\"delete\" type=\"submit\" class=\"btn btn-secondary\">Delete</button></form></td>\n";
                       echo "</tr>\n";
 
                 }
