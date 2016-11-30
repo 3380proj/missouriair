5533d53b874bf1e6b741831a2e3c7e706b76986d
akauble <austinkauble@gmail.com>
2016-11-29 19:42:52 -0600

    .

--- databaseProject/admin_delete.php.php
+++ databaseProject/admin_delete.php.php
@@ -12,26 +12,31 @@
                     case "certification":
                         $stmt = mysqli_prepare($conn, "DELETE FROM certification WHERE emp_id = ?");
                         mysqli_stmt_bind_param($stmt, "i", $value);
+                        log_event($conn, "CERTIFY", "Removed certification from pilot {$value}", null, null, $_SESSION['admin']);
                         break;
                         
                     case "customer":
                         $stmt = mysqli_prepare($conn, "DELETE FROM customer WHERE id = ?");
                         mysqli_stmt_bind_param($stmt, "i", $value);
+                        log_event($conn, "CUSTOMER", "Removed customer {$value}", null, null, $_SESSION['admin']);
                         break;
                         
                     case "employee":
                         $stmt = mysqli_prepare($conn, "DELETE FROM employee WHERE emp_id = ?");
                         mysqli_stmt_bind_param($stmt, "i", $value);
+                        log_event($conn, "TERMINATE", "Terminated employee {$value}", null, null, $_SESSION['admin']);
                         break;
                         
                     case "equipment":
                         $stmt = mysqli_prepare($conn, "DELETE FROM equipment WHERE serial LIKE ?");
                         mysqli_stmt_bind_param($stmt, "s", $value);
+                        log_event($conn, "EQUIP", "Removed equipment {$value}", null, null, $_SESSION['admin']);
                         break;  
                         
                     case "flight":
                         $stmt = mysqli_prepare($conn, "DELETE FROM flight WHERE number = ?");
                         mysqli_stmt_bind_param($stmt, "i", $value);
+                        log_event($conn, "FLIGHT", "Cancelled flight {$value}", null, null, $_SESSION['admin']);
                         break;
          
                 }
--- databaseProject/log.php
+++ databaseProject/log.php
@@ -80,6 +80,7 @@
             
             Type of Action:
             <select name="action_type">
+                <option value = ""></option>
                 <option value = "RESERVE">Reservation</option>
                 <option value = "CERTIFY">Certification</option>
             </select>
